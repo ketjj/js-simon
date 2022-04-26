eedf91dc83c003b9ad3b40e777b57040347803b9
ketjj <jorjolianiket@gmail.com>
2022-04-27 01:32:40 +0200

    adding arrays

--- js/script.js
+++ js/script.js
@@ -7,7 +7,8 @@
 const container = document.getElementById('upper');
 const grids = createBoxes();
 
-
+let printNumbers = [];
+// CREARE GRID
 
 function createBoxes(){
   for(let i = 0; i < 5; i++){
@@ -20,8 +21,22 @@ function createElement(){
   box.className = 'box';
   box.innerHTML = getRandomNumber(1, 50);
   container.append(box);
+  console.log(box.innerText);
+
+  let printNumbers = [];
+
+  let flag = false;
+  while (!flag)
+  if (!printNumbers.includes(box.innerText)) {
+    printNumbers.push(parseInt(box.innerText));
+    flag = true;
+  }else{
+    printNumbers.innerText = generateRandomNumber(1, 50);
+  }
 }
 
+
+//SET TIME OUT
 
 const myTimeout = setTimeout(myNumbers, 5000);
 
@@ -29,24 +44,54 @@ function myNumbers(){
   let insertedNumber = getNumberprompt();
 }
 
+function myStopFunction() {
+  clearInterval(myTimeout);
+}
+
+// Fare dei controlli ai numeri inseriti dal prompt
 
 function getNumberprompt(){
 
   for(let i = 0; i < 5; i++){
     
-    let insertNumber = prompt('Inserire ' + (i+1) + '° numero memorizzato da 1 a 50');
+    let insertNumber = prompt('Inserire ' + (i+1) + '° numero memorizzato su 5');
 
-    while (isNaN(insertNumber)){
-      alert('Non ha inserito un numero... riprova');
-      insertNumber = prompt('Inserire ' + (i+1) + '° numero memorizzato da 1 a 50');
+    numGuessArray.push(insertNumber);
+    
+    while (isNaN(insertNumber) || (insertNumber > 50)){
+      alert('Devi inserire solo un numero da 1 a 50, riprova!');
+      insertNumber = prompt('Inserire ' + (i+1) + '° numero memorizzato su 5');
+    }
+    
+
+    let flag = false;
+
+    if(!numGuessArray.includes(insertNumber)){
+      numGuessArray.push(parseInt(insertNumber));
+      flag = true;
+    }else{
+      flag = false;
     }
 
-    insertNumber = parseInt(insertNumber);
+    insertNumber = parseInt(insertNumber).value;
+    
   }
 }
 
+// HO generato array prompt
+const numGuessArray = [];
+console.log(numGuessArray);
 
+
+//GEnera i numeri random
 
 function getRandomNumber(min, max){
   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+document.querySelector('button').addEventListener('click', reset);
+
+function reset(){
+  // box.innerHTML = '';
 }
